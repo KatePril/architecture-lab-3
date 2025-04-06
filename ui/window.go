c5a741ef884f6e0868349431c2140b36c7cfa803
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-04-06 20:01:51 +0300

    Add right mouse click event handling

--- ui/window.go
+++ ui/window.go
@@ -117,13 +117,18 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 
 	case mouse.Event:
 		if t == nil {
-			// TODO: Реалізувати реакцію на натискання кнопки миші.
+			if e.Button == mouse.ButtonRight {
+				log.Printf("Clicked: %v, %v", e.X, e.Y)
+				x := int(e.X)
+				y := int(e.Y)
+				pw.drawDefaultUI(&x, &y)
+			}
 		}
 
 	case paint.Event:
 		// Малювання контенту вікна.
 		if t == nil {
-			pw.drawDefaultUI()
+			pw.drawDefaultUI(nil, nil)
 		} else {
 			// Використання текстури отриманої через виклик Update.
 			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
@@ -132,23 +137,21 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 	}
 }
 
-func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.RGBA{R: 174, G: 255, B: 168}, draw.Src) // Фон.
+func (pw *Visualizer) drawDefaultUI(x, y *int) {
+	pw.w.Fill(pw.sz.Bounds(), color.RGBA{R: 174, G: 255, B: 168}, draw.Src)
 
-	//pw.w.Fill(pw.sz.Bounds(), color.RGBA{R: 105, G: 255, B: 94}, draw.Src)
-	x := pw.sz.WidthPx / 2
-	y := pw.sz.HeightPx / 2
-	log.Printf("%f", x)
-	log.Printf("%f", y)
-	pw.drawShape(x, y)
+	if x == nil {
+		defaultX := pw.sz.WidthPx / 2
+		x = &defaultX
+	}
+	if y == nil {
+		defaultY := pw.sz.HeightPx / 2
+		y = &defaultY
+	}
 
-	//rect := image.Rect(50, 50, 200, 150) // x0, y0, x1, y1
-	//pw.w.Fill(rect, color.RGBA{R: 255, G: 230, B: 69}, draw.Src)
-	//w, h := 100, 50
-	//x, y := 200, 200
-	//rotatedRect := image.Rect(x-h, y, x, y+w)
-	//pw.w.Fill(rotatedRect, color.RGBA{R: 255, G: 230, B: 69}, draw.Src)
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	log.Printf("%f", *x)
+	log.Printf("%f", *y)
+	pw.drawShape(*x, *y)
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
@@ -160,18 +163,19 @@ func (pw *Visualizer) drawShape(x, y int) {
 	s := 200
 	shapeColor := color.RGBA{R: 255, G: 230, B: 69}
 
-	x0_0 := x - s
-	y0_0 := y - s
-	x1_0 := x + s
-	y1_0 := y
-	rect_0 := image.Rect(x0_0, y0_0, x1_0, y1_0)
+	rotate := func(px, py int) (int, int) {
+		px, py = px-x, py-y
+		return x + py, y - px
+	}
 
-	x0_1 := x - (s / 2)
-	y0_1 := y
-	x1_1 := x + (s / 2)
-	y1_1 := y + s
-	rect_1 := image.Rect(x0_1, y0_1, x1_1, y1_1)
+	x00, y00 := rotate(x-s, y-s)
+	x10, y10 := rotate(x+s, y)
+	rect0 := image.Rect(x00, y00, x10, y10)
 
-	pw.w.Fill(rect_0, shapeColor, draw.Src)
-	pw.w.Fill(rect_1, shapeColor, draw.Src)
+	x01, y01 := rotate(x-(s/2), y)
+	x11, y11 := rotate(x+(s/2), y+s)
+	rect1 := image.Rect(x01, y01, x11, y11)
+
+	pw.w.Fill(rect0, shapeColor, draw.Src)
+	pw.w.Fill(rect1, shapeColor, draw.Src)
 }
