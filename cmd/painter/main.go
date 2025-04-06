a544a9fb736aa5585cd22f18c61f3835cf01698a
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-04-06 12:12:39 +0300

    Update imports

--- cmd/painter/main.go
+++ cmd/painter/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"net/http"
 
-	"github.com/roman-mazur/architecture-lab-3/painter"
-	"github.com/roman-mazur/architecture-lab-3/painter/lang"
-	"github.com/roman-mazur/architecture-lab-3/ui"
+	"github.com/KatePril/architecture-lab-3/painter"
+	"github.com/KatePril/architecture-lab-3/painter/lang"
+	"github.com/KatePril/architecture-lab-3/ui"
 )
 
 func main() {
