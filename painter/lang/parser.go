5d194bf3197685d7af5ae0df37d9437e63ed0ed3
Andrii-Detix <aivancisin6@gmail.com>
2025-04-13 00:33:10 +0300

    Add setOperationDictionary function

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -3,10 +3,10 @@ package lang
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"strings"
-
 	"github.com/KatePril/architecture-lab-3/painter"
+	"io"
+	"strconv"
+	"strings"
 )
 
 type Parser interface {
@@ -71,4 +71,86 @@ func (p *OperationParser) parseLine(commandLine string) (painter.Operation, erro
 	}
 
 	return operation, nil
+}
+
+func setOperationDictionary(state *painter.State) map[string]GetOperationFunc {
+	var dictionary map[string]GetOperationFunc
+
+	dictionary["update"] = func(args []string) (painter.Operation, error) {
+		return painter.UpdateOp, nil
+	}
+
+	dictionary["white"] = func(args []string) (painter.Operation, error) {
+		return painter.MakeWhiteFillOp(state), nil
+	}
+
+	dictionary["green"] = func(args []string) (painter.Operation, error) {
+		return painter.MakeGreenFillOp(state), nil
+	}
+
+	dictionary["bgrect"] = func(args []string) (painter.Operation, error) {
+		const rightNumOfArgs int = 4
+		if len(args) != rightNumOfArgs {
+			return nil, fmt.Errorf("invalid arguments")
+		}
+
+		var parsedArgs [rightNumOfArgs]int
+
+		for i := 0; i < rightNumOfArgs; i++ {
+			parsed, err := strconv.Atoi(args[i])
+			if err != nil {
+				return nil, fmt.Errorf("invalid arguments")
+			}
+
+			parsedArgs[i] = parsed
+		}
+
+		return painter.MakeBgRectOp(state, parsedArgs[0], parsedArgs[1], parsedArgs[2], parsedArgs[3]), nil
+	}
+
+	dictionary["figure"] = func(args []string) (painter.Operation, error) {
+		const rightNumOfArgs int = 2
+		if len(args) != rightNumOfArgs {
+			return nil, fmt.Errorf("invalid arguments")
+		}
+
+		var parsedArgs [rightNumOfArgs]int
+
+		for i := 0; i < rightNumOfArgs; i++ {
+			parsed, err := strconv.Atoi(args[i])
+			if err != nil {
+				return nil, fmt.Errorf("invalid arguments")
+			}
+
+			parsedArgs[i] = parsed
+		}
+
+		return painter.MakeFigureOp(state, parsedArgs[0], parsedArgs[1]), nil
+	}
+
+	dictionary["move"] = func(args []string) (painter.Operation, error) {
+		const rightNumOfArgs int = 2
+		if len(args) != rightNumOfArgs {
+			return nil, fmt.Errorf("invalid arguments")
+		}
+
+		var parsedArgs [rightNumOfArgs]int
+
+		for i := 0; i < rightNumOfArgs; i++ {
+			parsed, err := strconv.Atoi(args[i])
+			if err != nil {
+				return nil, fmt.Errorf("invalid arguments")
+			}
+
+			parsedArgs[i] = parsed
+		}
+
+		return painter.MakeMoveOp(state, parsedArgs[0], parsedArgs[1]), nil
+	}
+
+	dictionary["reset"] = func(args []string) (painter.Operation, error) {
+		return painter.MakeResetOp(state), nil
+	}
+
+	return dictionary
 }
