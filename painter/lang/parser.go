90a4dc55a654084f219f1beae221ddbe25535805
Andrii-Detix <aivancisin6@gmail.com>
2025-04-12 23:22:14 +0300

    Add Parse method body

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -1,6 +1,7 @@
 package lang
 
 import (
+	"bufio"
 	"io"
 
 	"github.com/KatePril/architecture-lab-3/painter"
@@ -14,18 +15,31 @@ type OperationParser struct {
 }
 
 func (p *OperationParser) Parse(in io.Reader) ([]painter.Operation, error) {
-	var res []painter.Operation
-	state := &painter.State{}
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanLines)
 
-	res = append(res, painter.MakeWhiteFillOp(state))
-	res = append(res, painter.MakeBgRectOp(state, 55, 55, 75, 75))
-	res = append(res, painter.MakeFigureOp(state, 201, 165))
-	res = append(res, painter.MakeGreenFillOp(state))
-	res = append(res, painter.MakeFigureOp(state, 300, 300))
-	res = append(res, painter.MakeBgRectOp(state, 60, 100, 300, 300))
-	res = append(res, painter.MakeMoveOp(state, 100, 100))
-	res = append(res, painter.MakeResetOp(state))
-	res = append(res, painter.UpdateOp)
+	var res []painter.Operation
+
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		commandLine := scanner.Text()
+		operation, err := p.parseLine(commandLine)
+		if err != nil {
+			return nil, err
+		}
+		if operation == nil {
+			continue
+		}
+
+		res = append(res, operation)
+	}
 
 	return res, nil
+}
+
+func (p *OperationParser) parseLine(commandLine string) (painter.Operation, error) {
+	return nil, nil
 }
