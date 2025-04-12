0b043e187a88a73a9daff75bf9a7a7b6ad140201
Andrii-Detix <aivancisin6@gmail.com>
2025-04-12 23:16:27 +0300

    Add Parser interface

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -6,11 +6,14 @@ import (
 	"github.com/KatePril/architecture-lab-3/painter"
 )
 
-// Parser уміє прочитати дані з вхідного io.Reader та повернути список операцій представлені вхідним скриптом.
-type Parser struct {
+type Parser interface {
+	Parse(in io.Reader) ([]painter.Operation, error)
 }
 
-func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
+type OperationParser struct {
+}
+
+func (p *OperationParser) Parse(in io.Reader) ([]painter.Operation, error) {
 	var res []painter.Operation
 	state := &painter.State{}
 
